73a768ef5f059af38ffce74c396992e5b6e7f7de
Cameron <cameo221@gmail.com>
2025-06-06 12:23:52 -0700

    elemental type os ready. exploring render pipeline

--- Assets/_Game/Scripts/HudBarEvolution.cs
+++ Assets/_Game/Scripts/HudBarEvolution.cs
@@ -4,11 +4,17 @@ using UnityEngine.UI;
 
 public class HudBarEvolution : Singleton<HudBarEvolution>
 {
-    public Image fillImage;
+    public Image[] bars;
     
     //normal, fire, ice, electric
     public void UpdateBars(float[] weights)
     {
-        
+        float cumulativeWeight = 0f;
+
+        for (int i = 0; i < bars.Length; i++)
+        {
+            cumulativeWeight += weights[i];
+            bars[i].fillAmount = cumulativeWeight;
+        }
     }
 }
