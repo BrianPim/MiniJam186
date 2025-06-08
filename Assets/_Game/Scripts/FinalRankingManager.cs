38c2570240bb252e59237bde821eaddd381d7dc7
Cameron <cameo221@gmail.com>
2025-06-08 15:25:44 -0700

    made the final ranking thing

---
+++ Assets/_Game/Scripts/FinalRankingManager.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using CamLib;
+
+public class FinalRankingManager : Singleton<FinalRankingManager>
+{
+    public List<Ranking> Rankings = new List<Ranking>();
+        
+    [Serializable]
+    public class Ranking
+    {
+        public string Rank;
+        public int ScoreThreshold;
+    }
+
+    public string GetRanking(int score)
+    {
+        // Find the highest ranking that the score qualifies for
+        string currentRank = Rankings[0].Rank; // Default to lowest rank
+            
+        for (int i = Rankings.Count - 1; i >= 0; i--)
+        {
+            if (score >= Rankings[i].ScoreThreshold)
+            {
+                currentRank = Rankings[i].Rank;
+                break;
+            }
+        }
+
+        return currentRank;
+    }
+}
