7d1b31b02f85e582d0a84958d92475a420de0017
Cameron <cameo221@gmail.com>
2025-06-08 14:14:23 -0700

    anim

---
+++ Assets/_Game/Scripts/TutorialManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    /// <summary>
+    /// Show how to move.
+    /// Show how to shoot.
+    /// Show how to switch weapons.
+    /// </summary>
+    public class TutorialManager : MonoBehaviour
+    {
+        
+    }
+}
